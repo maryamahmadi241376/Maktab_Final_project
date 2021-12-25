0dce1f30403bc7757176bcdf41b932960dd78ab2
maryam.ahmadi241376 <maryam.ahmadi241376@gmai.com>
2021-12-25 14:08:30 +0330

    database changed to postgres

--- config/settings.py
+++ config/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'final_project',
+        'USER': 'postgres',
+        'PASSWORD': 'mn23241376',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
