06712a5949c6ccc6258d125e65e3de7b321b7c13
maryam.ahmadi241376 <maryam.ahmadi241376@gmai.com>
2021-12-27 21:07:30 +0330

    2 forms created in forms file

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,14 +1,14 @@
-# from .models import CustomUser
-# from django.contrib.auth.forms import UserCreationForm,UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import *
 
-# class CustomUserCreationForm(UserCreationForm):
 
-#     class Meta:
-#         model = CustomUser
-#         fields = ('email','username',)
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm): 
+        model = CustomUser 
+        fields = ('email', 'age',)
 
-# class CustomUserChangeForm(UserChangeForm):
 
-#     class Meta:
-#         model = CustomUser
-#         fields = ('email','username',)
+class CustomUserChangeForm(UserChangeForm):
+    class Meta: 
+        model = CustomUser
+        fields = ('email', 'age',) 
