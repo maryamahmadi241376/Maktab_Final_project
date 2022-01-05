c22a21d2d4a5a47cce652dd5aa4f6b4a89712e2e
maryam.ahmadi241376 <maryam.ahmadi241376@gmai.com>
2022-01-05 15:19:26 +0330

    most seller restaurants and foods

--- orderonline/urls.py
+++ orderonline/urls.py
@@ -1,5 +1,19 @@
 from django.urls import path
+from .views import *
 
-urlpatterns = [
+
+
+urlpatterns=[
     
-]
+    path('foods/',foodView,name="foods"),
+    path('',show_foods,name="home"),
+    # path('food_form/',FoodCreate.as_view(),name='add_food'),
+    # path('delete_food/<int:pk>/', FoodDelete.as_view(), name='delete_food'),
+    # path('edit_food/<int:pk>/', FoodUpdate.as_view(), name='edit_food'),
+    # path('food_category/add/', PostFoodCategoryCreate.as_view(), name='add_food_category'),
+    # path('cart/', cart, name="add_to_cart"),
+    # path('delete_orderitem/<int:pk>/',OrderItemDeleteView.as_view(),name="delete_orderitem"),
+    # path('orders_detail/<int:pk>/',food,name="order_detail"),
+    # # path('food_form/',addfood,name='add_food'),
+    # path("address/",AddressView.as_view(),name="address")
+    ]
