268c841672db835895f8ee639e501e2d9adbd84c
maryam.ahmadi241376 <maryam.ahmadi241376@gmai.com>
2022-01-05 15:43:42 +0330

    orderonline models updated/admin panel customized

--- orderonline/admin.py
+++ orderonline/admin.py
@@ -1,10 +1,79 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(RestaurantBranch)
-admin.site.register(Food)
-admin.site.register(Menu)
 admin.site.register(Restaurant)
 admin.site.register(MealCategory)
 admin.site.register(FoodCategory)
-admin.site.register(Order)
+admin.site.register(Menu)
+
+
+
+
+
+
+@admin.register(RestaurantBranch)
+class CustomRestaurantBranch(admin.ModelAdmin):
+    model = RestaurantBranch
+    list_display = ['restaurant_branch_name','address','restaurant_branch_status',]
+    list_editable = ['restaurant_branch_status',]
+    empty_value_display = 'is null'
+    list_filter = ['restaurant_branch_name','address','restaurant_branch_status',]
+    list_per_page = 4
+    search_fields = ['restaurant_branch_name','address','restaurant_branch_status',]
+
+@admin.register(OrderItem)
+class CustomMenu(admin.ModelAdmin):
+    model = OrderItem
+    list_display = ['number',]
+    empty_value_display = 'is null'
+    list_per_page = 4
+    search_fields = ['number',]
+    fieldsets = (
+            (None, {
+                "fields": (
+                    'order','menu','number',
+                ),
+            }),
+        )
+
+@admin.register(Food)
+class CustomFood(admin.ModelAdmin):
+    model = Food
+    list_display = ['food_name','food_image','food_description',]
+    list_editable = ['food_description',]
+    empty_value_display = 'is null'
+    list_filter = ['food_name',]
+    list_per_page = 4
+    search_fields = ['food_name',]
+    fieldsets = (
+            (None, {
+                "fields": (
+                    'food_name','food_image','food_category_id','meal',
+                ),
+            }),
+            ('personal info', {
+                "description": 'This is food information :)',
+                "classes": ('collapse',),
+                "fields": (
+                    'food_description',
+                ),
+                }
+            ),
+        )
+
+@admin.register(Order)
+class CustomOrder(admin.ModelAdmin):
+    model = Order
+    list_display = ['delivery_time','customer_status',]
+    list_editable = ['customer_status',]
+    empty_value_display = 'is null'
+    list_filter = ['customer_status',]
+    list_per_page = 4
+    search_fields = ['delivery_time','customer_status',]
+    fieldsets = (
+            (None, {
+                "fields": (
+                    'order_item','total_price','customer','address','branch','customer_status',
+                ),
+            }),
+        )
