d2fe7e1e9a7d77b632a0b5fa3121705e30947771
maryam.ahmadi241376 <maryam.ahmadi241376@gmai.com>
2022-01-12 23:22:53 +0330

    customer, address and manager related urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,9 +6,22 @@ urlpatterns = [
     
     # path('managers_signup/',managers_signup,name="managers_signup"),
     # path('add_admin/',AdminCreate.as_view(),name = 'add_admin'),
-    path('add_manager/',ManagerCreate.as_view(),name = 'add_manager'),
-    path('add_customer/',AddressCreate.as_view(),name = 'add_address'),
-    path('customer/',CustomerCreate.as_view(),name = 'add_customer'),
-    path('address/',address_create)
     
+    #customer
+    # path('add_customer/',AddressCreate.as_view(),name = 'add_address'),
+    path('customer/',customer_signup,name = 'add_customer'),
+    path('customeredit/<int:pk>/',CustomerEdit.as_view(),name="customer_edit"),
+    path('signout/<int:pk>/',CustomerSignOut.as_view(),name="customer_signout"),
+
+    #address
+    path('change_main_address/<int:pk>/',change_main_address,name="change_main_address"),
+    path('delete_address/<int:pk>/',delete_address,name="delete_address"),
+    path('address/',address_create,name = "addaddress"),
+    
+
+    #manager
+    path('branch_edit/<int:pk>/',BranchManagerEdit.as_view(),name="branch_edit"),
+    path('manager_edit/<int:pk>/',ManagerEdit.as_view(),name="manager_edit"),
+    path('manager_signup/',manager_signup,name="add_manager"),
+
 ]
