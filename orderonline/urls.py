1afa073399df6f87910544372543ddde2d9b94ab
maryam.ahmadi241376 <maryam.ahmadi241376@gmai.com>
2022-01-12 23:34:06 +0330

    customer and restaurant panel urls created

--- orderonline/urls.py
+++ orderonline/urls.py
@@ -4,16 +4,35 @@ from .views import *
 
 
 urlpatterns=[
-    
-    path('foods/',foodView,name="foods"),
+    #websitemanager
+    path('foods/',FoodView.as_view(),name="foods"),
     path('',show_foods,name="home"),
     path('food_form/',FoodCreate.as_view(),name='add_food'),
     path('delete_food/<int:pk>/', FoodDelete.as_view(), name='delete_food'),
     path('edit_food/<int:pk>/', FoodUpdate.as_view(), name='edit_food'),
     path('food_category/add/', PostFoodCategoryCreate.as_view(), name='add_food_category'),
+
+    #cart
     path('cart/', cart, name="add_to_cart"),
     path('delete_orderitem/<int:pk>/',OrderItemDeleteView.as_view(),name="delete_orderitem"),
     path('orders_detail/<int:pk>/',food,name="order_detail"),
-    # # path('food_form/',addfood,name='add_food'),
-    # path("address/",AddressView.as_view(),name="address")
+
+    #restaurant
+    
+
+
+    path('restaurant_panel',ManagerMenus.as_view(),name="restaurant_panel"),
+    path("create_menu/",MenuCreate.as_view(),name="create_menu"),
+    path("delete_menu/<int:pk>/",MenuDelete.as_view(),name="delete_menu"),
+    path("edit_menu/<int:pk>/",MenuUpdate.as_view(),name="edit_menu"),
+
+
+    # search
+    path('search/',search,name="search"),
+
+
+    #customer
+    path('customer_panel/',CustomerOrders.as_view(),name="customer_panel"),
+
+    
     ]
